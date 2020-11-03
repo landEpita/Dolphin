cabecd7a3aa4e6c820cd12eb70c8fb4c39ac1005
Thomas gossin <Thomas.gossin@epita.fr>
2020-11-03 20:20:24 +0100

    recherche

--- src/Manager.py
+++ src/Manager.py
@@ -1,4 +1,6 @@
 import requests
+import pandas as pd
+import json
 
 class RESTManager:
     HOST_NAME = "dolphin.jump-technology.com"
@@ -17,3 +19,24 @@ class RESTManager:
     
     def post(self, path):
         return requests.post(self.URL+path, auth=self.auth, verify=False).text
+    
+    def get_asset(self):
+        dt_text = self.get("asset")
+        dt = json.loads(dt_text)
+        df = []
+        for i in range(len(dt)):
+            tmp = {}
+            for key in dt[i]:
+                tmp[key] = dt[i][key]["value"]
+            df.append(tmp)
+        df = pd.DataFrame(df)
+        return df
+    
+    def get_asset_id(self, uid):
+        dt_text = self.get(f"asset/{uid}")
+        dt = json.loads(dt_text)
+        df={}
+        for key in dt:
+            df[key] = dt[key]["value"]
+        #df = pd.DataFrame(df)
+        return df
